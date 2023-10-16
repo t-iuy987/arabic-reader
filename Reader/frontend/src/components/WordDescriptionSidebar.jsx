55d3cce09f4c6581295e037f7b9025144c5e345c
t-iuy987 <m.urvah01@gmail.com>
2023-10-16 21:08:01 +0500

    Added GetTags API

--- Reader/frontend/src/components/WordDescriptionSidebar.jsx
+++ Reader/frontend/src/components/WordDescriptionSidebar.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState,  useEffect} from 'react';
 import "../styles/wordDescriptionSidebar.css";
+import {getTags} from "../services/wordService.js"
 
 const WordDescriptionSidebar = ({ wordDetails, onClose }) => {
     
   // Check if wordDetails is empty (word not found)
   const isWordNotFound = !wordDetails.word;
+
+  // State to store the retrieved tags
+  const [tags, setTags] = useState([]);
+
+  // Function to fetch and set the tags
+  const getTagsAction = async () => {
+    try {
+      const tags = await getTags(wordDetails.word);
+      setTags(tags.split('|')); // Split tags by pipe separator
+      
+      setTags(tags.replace(/-/g, '').replace(/\|/g, ', '));
+    } catch (error) {
+      console.error('Error fetching tags:', error);
+      setTags([]);
+    }
+  };
+
+  // Fetch tags when the component mounts or when wordDetails changes
+  useEffect(() => {
+    getTagsAction();
+  }, [wordDetails]);
 
   return (
     <div  className={` wsidebar ${isWordNotFound ? 'not-found' : ''}`}>
@@ -19,7 +41,9 @@ const WordDescriptionSidebar = ({ wordDetails, onClose }) => {
           <h2>لفظ: {wordDetails.word}</h2>
           <p>معنی: {wordDetails.definition}</p>
           <p>جذر: {wordDetails.root}</p>
-          <p>علامات: {wordDetails.tags}</p>
+          <p>علامات: {tags}</p>
+          
+          
         </div>
       )}
     </div>
--- Reader/frontend/src/services/wordService.js
+++ Reader/frontend/src/services/wordService.js
@@ -16,4 +16,25 @@ export const getWordMeaning = async (word) => {
     throw error; // errors
   }
 };
-export default getWordMeaning;
+
+// API function to get words with the same root as 'word'
+export const getWordsWithSameRoot = async (word) => {
+  try {
+    const response = await api.get(`words/GetWordsWithSameRoots?word=${word}`);
+    return response.data;
+  } catch (error) {
+    console.error('API error:', error);
+    throw error; // errors
+  }
+};
+
+// API function to get words with the same root as 'word'
+export const getTags = async (word) => {
+  try {
+    const response = await api.get(`words/GetTags?word=${word}`);
+    return response.data;
+  } catch (error) {
+    console.error('API error:', error);
+    throw error; // errors
+  }
+};
