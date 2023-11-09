7ea53dd55879e0d9aabd7d12d4ebe7c8707ffaeb
t-iuy987 <urvahm01@gmail.com>
2023-11-09 11:27:30 +0500

    Update TextSelectionMenu.jsx

--- Reader/frontend/src/components/TextSelectionMenu.jsx
+++ Reader/frontend/src/components/TextSelectionMenu.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 
 
-const TextSelectionMenu = ({ show, position, onGetTags, onfindRoot, onAdd, onDescribe }) => {
+const TextSelectionMenu = ({ show, position, onCopy, onfindRoot, onAdd, onDescribe }) => {
   const menuStyle = {
-    display: show ? 'block' : 'none',
+   // display: show ? 'block' : 'none',
     position: 'absolute',
     top: `${position.y}px`,
     left: `${position.x}px`,
     backgroundColor: 'white',
     border: '1px solid #ccc',
+    borderRadius:'7px',
     padding: '10px',
     zIndex: 9999, // Ensure it appears above other content
   };
 
   const buttonStyle = {
-    backgroundColor: '#007bff',
+    backgroundColor: '#e0d2c0',
     color: 'white',
     border: 'none',
+    borderRadius:'7px',
     padding: '5px 10px',
     margin: '5px',
     cursor: 'pointer',
@@ -25,8 +27,8 @@ const TextSelectionMenu = ({ show, position, onGetTags, onfindRoot, onAdd, onDes
   return (
     
     <div style={menuStyle}>
-      <button style={buttonStyle} onClick={onGetTags}>Find Parts-of-Speech</button>
-      <button style={buttonStyle} onClick={onfindRoot}>Find Root</button>
+      {/*<button style={buttonStyle} onClick={onCopy}>Copy</button>*/}
+      <button style={buttonStyle} onClick={onfindRoot}>Similar Words</button>
       <button style={buttonStyle} onClick={onAdd}>❤ Favorite</button>
       <button style={buttonStyle} onClick={onDescribe}>Describe</button>
     </div>
